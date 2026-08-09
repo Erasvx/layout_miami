012745a218e1d5e037528a94ced466febace0246
Hubert Piasecki <piahub@wp.pl>
2026-08-09 17:28:02 +0200

    Almost whole site was made, programming address link to  maps, phone button with call

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,15 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // const name = document.getElementById('name').value;
+  // const email = document.getElementById('email').value;
+  // const message = document.getElementById('message').value;
+  // To w razie W na przyszlosc sobie zostawilem :)
+
+  alert("Form has been sent! We will contact you as fast as it will be possible!");
+  e.target.reset();
+});
